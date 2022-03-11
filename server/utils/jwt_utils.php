2dd063c60f0ea0b990b5427d547174a8827f2eab
Quân Lưu <ekkott7@gmail.com>
2022-03-11 17:48:21 +0700

    cap nhat file jwt

--- server/utils/jwt_utils.php
+++ server/utils/jwt_utils.php
@@ -66,11 +66,11 @@ function get_authorization_header()
 
 	if (isset($_SERVER['Authorization'])) {
 		$headers = trim($_SERVER["Authorization"]);
-	} else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { //Nginx or fast CGI
+	} else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { 
 		$headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
 	} else if (function_exists('apache_request_headers')) {
 		$requestHeaders = apache_request_headers();
-		// Server-side fix for bug in old Android versions (a nice side-effect of this fix means we don't care about capitalization for Authorization)
+		
 		$requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
 		//print_r($requestHeaders);
 		if (isset($requestHeaders['Authorization'])) {
